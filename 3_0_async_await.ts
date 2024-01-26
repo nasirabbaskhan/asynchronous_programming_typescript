f94a1898a4cbcea83a618197973171233c449429
nasirabbaskhan <akhtarabbas.islamicreaction@gmail.com>
2024-01-26 00:05:49 -0800

    async-await in typescript

---
+++ 3_0_async_await.ts
@@ -0,0 +1,26 @@
+function processData() {
+  return new Promise((resolve, reject) => {
+    console.log("Data is processing");
+    let data: string;
+    setTimeout(() => {
+      if (data) {
+        resolve(data);
+      } else {
+        reject("Data is not fetched");
+      }
+    }, 1000);
+  });
+}
+
+async function fetchData() {
+  try {
+    let data = await processData();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+console.log("before");
+fetchData();
+console.log("after");
